b679b30c8cfc1adb5b06ac737ba424df1c5478cc
hacxy <hacxy.js@outlook.com>
2024-10-26 11:37:00 +0800

    fix: 修复了输入array类型时,只转化了元素类型没有将顶层array类型转化

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,16 @@
 #!/usr/bin/env node
-import JsonToTs from 'json-to-ts';
-import * as Codegen from '@sinclair/typebox-codegen';
 import clipboard from 'clipboardy';
+import { json2typebox } from 'json2typebox';
 import logSymbols from 'log-symbols';
-
-const bootstrap = () => {
-  const clipboardValue = clipboard.readSync();
-
-  let typeText = '';
+const bootstrap = async () => {
+  const clipboardStr = clipboard.readSync();
   try {
-    JsonToTs(JSON.parse(clipboardValue), { rootName: 'Root', useTypeAlias: true }).forEach((typeInterface) => {
-      typeText += typeInterface;
-    });
+    const code = await json2typebox(clipboardStr, 'Root');
+    clipboard.writeSync(code);
+    console.log(logSymbols.success, 'Typescript类型转化TypeBox类型成功, 已复制至粘贴板~');
   } catch (error) {
-    console.log(logSymbols.error, '转化失败, 请检查粘贴板数据是否正确');
-    process.exit(1);
+    console.log(logSymbols.error, '转化失败, 请检查粘贴板内容是否为json格式');
   }
-  const code = Codegen.TypeScriptToTypeBox.Generate(typeText);
-  const finalCode = Codegen.Formatter.Format(code);
-  clipboard.writeSync(finalCode);
-  console.log('');
-  console.log(logSymbols.success, 'Typescript类型转化TypeBox类型成功, 已保存至粘贴板~');
 };
 
 bootstrap();
