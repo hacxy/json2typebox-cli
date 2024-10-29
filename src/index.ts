998295a1c837d5f8b85d919f6ffc681d6412af4c
hacxy <hacxy.js@outlook.com>
2024-10-29 10:33:15 +0800

    feat: Support conversion ts type

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,35 @@
 import clipboard from 'clipboardy';
 import { json2typebox } from 'json2typebox';
 import logSymbols from 'log-symbols';
+import { cac } from 'cac';
+import projectInfo from '../package.json';
+
 const bootstrap = async () => {
-  const clipboardStr = clipboard.readSync();
-  try {
-    const code = await json2typebox(clipboardStr, 'Root');
-    clipboard.writeSync(code);
-    console.log(logSymbols.success, 'Typescript类型转化TypeBox类型成功, 已复制至粘贴板~');
-  } catch (error) {
-    console.log(logSymbols.error, '转化失败, 请检查粘贴板内容是否为json格式');
-  }
+  const cli = cac('json2typebox');
+  cli.command('tb', 'Generate typebox schemas').action(async () => {
+    const clipboardStr = clipboard.readSync();
+    try {
+      const code = await json2typebox(clipboardStr, 'Root');
+      clipboard.writeSync(code);
+      console.log(logSymbols.success, 'Successfully converted into typebox, to clipboard!');
+    } catch (error) {
+      console.log(logSymbols.error, 'Failed, check whether the paste board data is json.');
+    }
+  });
+
+  cli.command('ts', 'Generate typescript types').action(async () => {
+    const clipboardStr = clipboard.readSync();
+    try {
+      const code = await json2typebox(clipboardStr, 'Root');
+      clipboard.writeSync(code);
+      console.log(logSymbols.success, 'Successfully converted into typescrit, to clipboard!');
+    } catch (error) {
+      console.log(logSymbols.error, 'Failed, check whether the paste board data is json.');
+    }
+  });
+  cli.help();
+  cli.version(projectInfo.version);
+  cli.parse(process.argv);
 };
 
 bootstrap();
