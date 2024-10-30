6fbae7567e9f651da37dcb0e71e3447832a9d7d4
hacxy <hacxy.js@outlook.com>
2024-10-30 11:18:37 +0800

    fix: Adjust the command name

--- src/index.ts
+++ src/index.ts
@@ -7,27 +7,19 @@ import projectInfo from '../package.json';
 
 const bootstrap = async () => {
   const cli = cac('json2typebox');
-  cli.command('tb', 'Generate typebox schemas').action(async () => {
-    const clipboardStr = clipboard.readSync();
-    try {
-      const code = await json2typebox(clipboardStr, 'Root');
-      clipboard.writeSync(code);
-      console.log(logSymbols.success, 'Successfully converted into typebox, to clipboard!');
-    } catch (error) {
-      console.log(logSymbols.error, 'Failed, check whether the paste board data is json.');
-    }
-  });
+  cli
+    .command('quick', 'Read JSON from the clipboard and generate TypeBox types into the clipboard.')
+    .action(async () => {
+      const clipboardStr = clipboard.readSync();
+      try {
+        const code = await json2typebox(clipboardStr, 'Root');
+        clipboard.writeSync(code);
+        console.log(logSymbols.success, 'Successfully converted into typebox, to clipboard!');
+      } catch (error) {
+        console.log(logSymbols.error, 'Failed, check whether the paste board data is json.');
+      }
+    });
 
-  cli.command('ts', 'Generate typescript types').action(async () => {
-    const clipboardStr = clipboard.readSync();
-    try {
-      const code = await json2typebox(clipboardStr, 'Root');
-      clipboard.writeSync(code);
-      console.log(logSymbols.success, 'Successfully converted into typescrit, to clipboard!');
-    } catch (error) {
-      console.log(logSymbols.error, 'Failed, check whether the paste board data is json.');
-    }
-  });
   cli.help();
   cli.version(projectInfo.version);
   cli.parse(process.argv);
